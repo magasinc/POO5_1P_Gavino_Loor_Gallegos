0d12e681f11914ef0eed633f7e03fb1fdf260395
josegavinovi <jagavino@espol.edu.ec>
2024-06-18 22:18:37 -0500

    craecion de clase  usuario

--- demo/target/classes/Usuario.java
+++ demo/target/classes/Usuario.java
@@ -1,3 +1,11 @@
 public class Usuario {
+    protected String nombre;
+    protected String apellido;
+    protected String correoElectronico;
+    public Usuario(String nombre, String apellido, String correo){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.correoElectronico = correo;
+}
 
 }
